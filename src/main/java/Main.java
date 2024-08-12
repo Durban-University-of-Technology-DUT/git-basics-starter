bc51ea43bebf28b2a7aab6af5826c571539350ed
Raydo Matthee <raydo@skunkworks.africa>
2024-08-12 12:16:52 +0200

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+// Main.java
+// Main Java application entry point (Placeholder)
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // TODO: Implement the main logic of your Java application here
+    }
+}

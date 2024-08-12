ac5c4387d2bb3c83c9d69d9ad33a0b9d15876a5f
Raydo Matthee <raydo@skunkworks.africa>
2024-08-12 12:24:07 +0200

    Create JavaHelper.java

---
+++ src/main/java/utils/JavaHelper.java
@@ -0,0 +1,9 @@
+public class JavaHelper {
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    // TODO: Add more utility functions as needed
+
+}
